14a90870c86316948c36f1a93f524e7c51ec9d77
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-27 16:51:45 -0500

    variables de sesion

--- Calandria/servidor.py
+++ Calandria/servidor.py
@@ -1,11 +1,13 @@
+from os import urandom
 from io import BytesIO
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from flask import redirect, url_for, request, send_file
 from conexiones import obtenerValores, leerString, cambioTexto
 from conexiones import nuevaReceta, cambiarReceta, eliminarReceta, insert, conexion
 from conexiones import listaASCII, leerCompuesto, leerGreenTire, exportarExcel
 
 app = Flask(__name__)
+app.secret_key = urandom(24)
 host = "192.168.1.35"
 cursor, cnxn = conexion()
 
@@ -20,16 +22,19 @@ def login():
 @app.route('/inicio', methods=['POST'])
 def entrada():
 	if request.form["user"] == "admin" and request.form["passwd"] == "12345":
+		session["user"] = request.form["user"]
+		session["passwd"] = request.form["passwd"]
 		return redirect(url_for('index'))
 	else:
 		return redirect(url_for('login'))
-		
+
 #############################################################################################################################
 
 @app.route('/inicio')
 def index():
+
 	lista, datos = [], {}
-	for i in range(0,299):
+	for i in range(1,300):
 		var = leerString(host,i,"Medida")
 		if len(var) > 1:
 			compuesto = leerCompuesto(host,i)
@@ -103,6 +108,15 @@ def exportar():
 	return send_file(output, attachment_filename="archivo.xlsx", as_attachment=True)
 
 #############################################################################################################################
+
+@app.route('/salir')
+def salir():
+	session.clear()
+	print("sesion destruida")
+	return redirect(url_for('login'))
+
+#############################################################################################################################
+
 
 if __name__ == '__main__':
 	app.run( debug=True, port=8000)
