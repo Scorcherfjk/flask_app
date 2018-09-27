bcb14e4169bdedb73040d0b9298d787b07b17c73
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-27 11:38:26 -0500

    mejorando conexiones

--- Calandria/database.py
+++ Calandria/database.py
@@ -1,11 +1,11 @@
 import pyodbc 
 
-def conexion():
+def dbcursor():
     server = 'DESKTOP-QV8E63C' 
     database = 'squeegee' 
     username = 'sa' 
     password = 'Controlsi' 
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER={};DATABASE={};UID={};PWD={}'
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER={0};DATABASE={1};UID={2};PWD={3}'
     .format(server,database,username,password))
     cursor = cnxn.cursor()
-    return cursor
+    return cursor, cnxn
